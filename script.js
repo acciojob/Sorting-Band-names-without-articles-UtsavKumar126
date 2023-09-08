6c9df17f9ac72c5b836aee863e99bf193056c772
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-08 09:04:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 //your code here
 let arr1 = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
-let arr2=arr1.map(f);
-
 let f = (element)=>{
 	if(element.substring(0,3).toLowerCase().equals("the"))
 		return s.substring(4,element.length);
@@ -11,6 +9,8 @@ let f = (element)=>{
 	else if(element.substring(0,2).toLowerCase().equals("an"))
 		return s.substring(3,element.length);
 }
+
+let arr2=arr1.map(f);
 
 let obj={};
 let j=0;
