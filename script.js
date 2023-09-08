195b5567d6456108d706d744349c112a05884c0e
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-08 09:11:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ let arr1 = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
 let f = (element)=>{
 	if(element.substring(0,3).toLowerCase()==='the')
-		return s.substring(4,element.length);
+		return s
+			element.substring(4,element.length);
 	else if(element.substring(0,1).toLowerCase==='a')
-		return s.substring(2,element.length);
+		return element.substring(2,element.length);
 	else if(element.substring(0,2).toLowerCase()==='an')
-		return s.substring(3,element.length);
+		return element.substring(3,element.length);
 }
 
 let arr2=arr1.map(f);
