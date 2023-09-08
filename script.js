2fa43dcbadcd9f2804b628e14402c7ffe0f5b72c
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-08 09:24:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-//your code here
 let arr1 = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
 let f = (element)=>{
 	if(element.substring(0,3).toLowerCase()==='the')
-		return s
-			element.substring(4,element.length);
+		return element.substring(4,element.length);
+
 	else if(element.substring(0,1).toLowerCase==='a')
 		return element.substring(2,element.length);
+
 	else if(element.substring(0,2).toLowerCase()==='an')
 		return element.substring(3,element.length);
+
 	else
 		return element;
 }
@@ -17,13 +18,13 @@ let arr2=arr1.map(f);
 
 let obj={};
 let j=0;
-for(j=0;j<arr.length;j++){
+for(j=0;j<arr1.length;j++){
 	obj[arr2[j]]=arr1[j];
 }
 arr2.sort();
 let k=0;
 for(k=0;k<arr1.length;k++){
-	Console.log(obj.get(arr2[i]));
+	console.log(obj[arr2[k]]);
 }
 
 
